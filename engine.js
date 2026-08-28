8928e78f264d4eb760ec105e5653352a36cc3bf3
Paweł Worzała <pawelworzala86@gmail.com>
2026-08-28 13:29:35 +0200

    render to texture to panel

--- engine.js
+++ engine.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById('my_Canvas');
-gl = canvas.getContext('experimental-webgl');
+gl = canvas.getContext('webgl2');
 
 var vertices = [
     -0.5, 0.5, 0.0,
@@ -23,10 +23,17 @@ gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
 
 var vertices2 = [
-    -0.5, 1.0, 0.0,
-    -0.5,-0.5, 0.0,
-     0.5,-0.5, 0.0,
-     0.5, 0.5, 0.0
+    -1.0, -1.0, 0.0,
+     1.0, -1.0, 0.0,
+     1.0,  1.0, 0.0,
+    -1.0,  1.0, 0.0
+];
+
+var coords2 = [
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0
 ];
 
 indices2 = [3,2,1,3,1,0];
@@ -34,6 +41,11 @@ indices2 = [3,2,1,3,1,0];
 var vertex_buffer2 = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer2);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices2), gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+var coords_buffer2 = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, coords_buffer2);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coords2), gl.STATIC_DRAW);
 gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
 var Index_Buffer2 = gl.createBuffer();
@@ -47,24 +59,32 @@ gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
 
 
-var vertCode =
-    'attribute vec3 coordinates;' +
-    'void main(void) {' +
-    ' gl_Position = vec4(coordinates, 1.0);' +
-    '}';
+var vertCode =`#version 300 es
+    layout(location = 0) in vec3 coordinates;
+    void main(void) {
+     gl_Position = vec4(coordinates, 1.0);
+    }`
 
 var vertShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertShader, vertCode);
 gl.compileShader(vertShader);
+if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(vertShader));
+}
 
-var fragCode =
-    'void main(void) {' +
-    ' gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);' +
-    '}';
+var fragCode =`#version 300 es
+precision mediump float;
+out vec4 outColor;
+    void main(void) {
+     outColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }`
 
 var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragShader, fragCode);
 gl.compileShader(fragShader);
+if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(fragShader));
+}
 
 var shaderProgram = gl.createProgram();
 gl.attachShader(shaderProgram, vertShader);
@@ -74,12 +94,13 @@ gl.useProgram(shaderProgram);
 
 
 
-var vertCode2 =`
-    attribute vec3 coordinates;
-    varying vec2 uv;
+var vertCode2 =`#version 300 es
+    layout(location = 0) in vec3 coordinates;
+    layout(location = 1) in vec2 coord;
+    out vec2 uv;
     void main(void) {
      gl_Position = vec4(coordinates, 1.0);
-     uv = coordinates.xy;
+     uv = coord;
     }`
 
 var vertShader2 = gl.createShader(gl.VERTEX_SHADER);
@@ -90,11 +111,13 @@ if (!gl.getShaderParameter(vertShader2, gl.COMPILE_STATUS)) {
 }
 
 
-var fragCode2 =`precision mediump float;
+var fragCode2 =`#version 300 es
+precision mediump float;
 uniform sampler2D tex;
-varying vec2 uv;
+out vec4 outColor;
+in vec2 uv;
     void main(void) {
-     gl_FragColor = texture2D(tex, uv);
+     outColor = texture(tex, uv);
     }`
 
 var fragShader2 = gl.createShader(gl.FRAGMENT_SHADER);
@@ -116,35 +139,75 @@ gl.linkProgram(shaderProgram2);
 
 
 
-var tex = gl.createTexture();
+const tex = gl.createTexture();
 gl.bindTexture(gl.TEXTURE_2D, tex);
-gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, canvas.width, canvas.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    canvas.width,
+    canvas.height,
+    0,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    null
+);
+
 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-var fbo = gl.createFramebuffer();
+
+const rbo = gl.createRenderbuffer();
+gl.bindRenderbuffer(gl.RENDERBUFFER, rbo);
+gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, canvas.width, canvas.height);
+
+const fbo = gl.createFramebuffer();
 gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
-gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex, 0);
+
+gl.framebufferTexture2D(
+    gl.FRAMEBUFFER,
+    gl.COLOR_ATTACHMENT0,
+    gl.TEXTURE_2D,
+    tex,
+    0
+);
+
+gl.framebufferRenderbuffer(
+    gl.FRAMEBUFFER,
+    gl.DEPTH_ATTACHMENT,
+    gl.RENDERBUFFER,
+    rbo
+);
+
+if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) !== gl.FRAMEBUFFER_COMPLETE) {
+    console.error("FBO incomplete");
+}
+
 
 
 
 
 function animate(time){
-    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+
     gl.clearColor(0.5, 0.5, 0.5, 0.9);
     gl.enable(gl.DEPTH_TEST);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(0,0,canvas.width,canvas.height);
+    
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
 
     gl.useProgram(shaderProgram);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
 
     var coord = gl.getAttribLocation(shaderProgram, "coordinates");
-    gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(coord);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(0);
 
+    
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
     gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
 
 
@@ -153,22 +216,26 @@ function animate(time){
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     gl.clearColor(0.5, 0.5, 0.5, 0.9);
     gl.enable(gl.DEPTH_TEST);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    //gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(0,0,canvas.width,canvas.height);
 
     gl.useProgram(shaderProgram2);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer2);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer2);
 
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, tex);
     gl.uniform1i(gl.getUniformLocation(shaderProgram2, "tex"), 0);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer2);
     var coord2 = gl.getAttribLocation(shaderProgram2, "coordinates");
-    gl.vertexAttribPointer(coord2, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(coord2);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(0);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, coords_buffer2);
+    var coords2 = gl.getAttribLocation(shaderProgram2, "coord");
+    gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(1);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer2);
     gl.drawElements(gl.TRIANGLES, indices2.length, gl.UNSIGNED_SHORT, 0);
 
     window.requestAnimationFrame(animate);
