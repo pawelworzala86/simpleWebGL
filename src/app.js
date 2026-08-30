644557c8aeff336a94ad40ede8ce94792e315850
Paweł Worzała <pawelworzala86@gmail.com>
2026-08-30 16:53:02 +0200

    separate model

--- src/app.js
+++ src/app.js
@@ -1,95 +1,13 @@
 import http from './http.js'
 import Shader from './webgl/shader.js'
+import Model from './webgl/model.js'
 
 var canvas = document.getElementById('my_Canvas')
 const gl = canvas.getContext('webgl2')
 
 
-const geometry1 = await http.get('/models/box.json','json')
-
-var vertex_buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(geometry1.position), gl.STATIC_DRAW);
-gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-var Index_Buffer = gl.createBuffer();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(geometry1.indices), gl.STATIC_DRAW);
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-
-
-const geometry2 = await http.get('/models/panel.json','json')
-
-var vertex_buffer2 = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer2);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(geometry2.position), gl.STATIC_DRAW);
-gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-var coords_buffer2 = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, coords_buffer2);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(geometry2.coord), gl.STATIC_DRAW);
-gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-var Index_Buffer2 = gl.createBuffer();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer2);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(geometry2.indices), gl.STATIC_DRAW);
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-
-
-
-
-
-const defaultShader = await Shader.create(gl,'default')
-const panelShader = await Shader.create(gl,'panel')
-/*
-var vertCode = await http.get('/shaders/default.vert')
-var vertShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertShader, vertCode);
-gl.compileShader(vertShader);
-if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(vertShader));
-}
-
-var fragCode = await http.get('/shaders/default.frag')
-var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragShader, fragCode);
-gl.compileShader(fragShader);
-if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(fragShader));
-}
-
-var shaderProgram = gl.createProgram();
-gl.attachShader(shaderProgram, vertShader);
-gl.attachShader(shaderProgram, fragShader);
-gl.linkProgram(shaderProgram);
-gl.useProgram(shaderProgram);
-
-
-
-var vertCode2 = await http.get('/shaders/panel.vert')
-var vertShader2 = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertShader2, vertCode2);
-gl.compileShader(vertShader2);
-if (!gl.getShaderParameter(vertShader2, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(vertShader2));
-}
-
-var fragCode2 = await http.get('/shaders/panel.frag')
-var fragShader2 = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragShader2, fragCode2);
-gl.compileShader(fragShader2)
-if (!gl.getShaderParameter(fragShader2, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(fragShader2));
-}
-
-var shaderProgram2 = gl.createProgram();
-gl.attachShader(shaderProgram2, vertShader2);
-gl.attachShader(shaderProgram2, fragShader2);
-gl.linkProgram(shaderProgram2);
-*/
-
+const boxModel = await Model.create(gl,'box.json','default')
+const panelModel = await Model.create(gl,'panel.json','panel')
 
 
 
@@ -223,26 +141,7 @@ function animate(time){
     
     gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
 
-    gl.useProgram(defaultShader.program);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-
-    var coord = gl.getAttribLocation(defaultShader.program, "coordinates");
-    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(0);
-
-
-    var _Pmatrix = gl.getUniformLocation(defaultShader.program, "Pmatrix");
-    var _Vmatrix = gl.getUniformLocation(defaultShader.program, "Vmatrix");
-    var _Mmatrix = gl.getUniformLocation(defaultShader.program, "Mmatrix");
-
-    gl.uniformMatrix4fv(_Pmatrix, false, proj_matrix);
-    gl.uniformMatrix4fv(_Vmatrix, false, view_matrix);
-    gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix);
-
-    
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
-    gl.drawElements(gl.TRIANGLES, geometry1.indices.length, gl.UNSIGNED_SHORT, 0);
+    boxModel.render(proj_matrix,view_matrix,mo_matrix,tex)
 
 
 
@@ -253,24 +152,7 @@ function animate(time){
     //gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(0,0,canvas.width,canvas.height);
 
-    gl.useProgram(panelShader.program);
-
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, tex);
-    gl.uniform1i(gl.getUniformLocation(panelShader.program, "tex"), 0);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer2);
-    var coord2 = gl.getAttribLocation(panelShader.program, "coordinates");
-    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(0);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, coords_buffer2);
-    var coords2 = gl.getAttribLocation(panelShader.program, "coord");
-    gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(1);
-
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer2);
-    gl.drawElements(gl.TRIANGLES, geometry2.indices.length, gl.UNSIGNED_SHORT, 0);
+    panelModel.render(proj_matrix,view_matrix,mo_matrix,tex)
 
     window.requestAnimationFrame(animate);
 }
---
+++ src/webgl/model.js
@@ -0,0 +1,74 @@
+import http from './../http.js'
+import Shader from './shader.js'
+
+export class Model{
+    constructor(gl){
+        this.gl = gl
+    }
+    static async create(gl,fileName,shaderName){
+        const model = new Model(gl)
+
+        const shader = await Shader.create(gl,shaderName)
+        model.shader = shader
+
+        const geometry = await http.get('/models/'+fileName,'json')
+        model.geometry = geometry
+
+        model.buffer = {}
+        
+        var vertex_buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(geometry.position), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        model.buffer.vertex = vertex_buffer
+
+        var coords_buffer2 = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, coords_buffer2);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(geometry.coord), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        model.buffer.coord = coords_buffer2
+        
+        var Index_Buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(geometry.indices), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+        model.buffer.index = Index_Buffer
+
+        return model
+    }
+    render(proj_matrix,view_matrix,mo_matrix,tex){
+        const { gl,shader } = this
+
+        gl.useProgram(shader.program);
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, tex);
+        gl.uniform1i(gl.getUniformLocation(shader.program, "tex"), 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer.vertex);
+        var coord = gl.getAttribLocation(shader.program, "coordinates");
+        gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(0);
+
+        if(this.buffer.coord){
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer.coord);
+            var coords2 = gl.getAttribLocation(shader.program, "coord");
+            gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(1);
+        }
+
+        var _Pmatrix = gl.getUniformLocation(shader.program, "Pmatrix");
+        var _Vmatrix = gl.getUniformLocation(shader.program, "Vmatrix");
+        var _Mmatrix = gl.getUniformLocation(shader.program, "Mmatrix");
+
+        if(_Pmatrix) gl.uniformMatrix4fv(_Pmatrix, false, proj_matrix);
+        if(_Vmatrix) gl.uniformMatrix4fv(_Vmatrix, false, view_matrix);
+        if(_Mmatrix) gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix);
+
+        
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.buffer.index);
+        gl.drawElements(gl.TRIANGLES, this.geometry.indices.length, gl.UNSIGNED_SHORT, 0);
+    }
+}
+
+export default Model
