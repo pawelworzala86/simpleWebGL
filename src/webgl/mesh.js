44c76c579ccca28da9558b55e50549f49b710d22
Paweł Worzała <pawelworzala86@gmail.com>
2026-09-06 12:16:34 +0200

    Mesh refactoring

--- src/webgl/mesh.js
+++ src/webgl/mesh.js
@@ -11,8 +11,24 @@ export class Mesh{
         mesh.geometry = geometry
 
         mesh.buffer = {}
+
+        for(const key of Object.keys(geometry)){
+            let type = gl.ARRAY_BUFFER
+            if(key==="indices") type = gl.ELEMENT_ARRAY_BUFFER
+            let array
+            if(key==="indices"){
+                array = new Uint16Array(geometry[key])
+            }else{
+                array = new Float32Array(geometry[key])
+            }
+            const buffer = gl.createBuffer();
+            gl.bindBuffer(type, buffer);
+            gl.bufferData(type, array, gl.STATIC_DRAW);
+            //gl.bindBuffer(type, null);
+            mesh.buffer[key] = buffer
+        }
         
-        var vertex_buffer = gl.createBuffer();
+        /*var vertex_buffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(geometry.position), gl.STATIC_DRAW);
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
@@ -28,7 +44,7 @@ export class Mesh{
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(geometry.indices), gl.STATIC_DRAW);
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-        mesh.buffer.index = Index_Buffer
+        mesh.buffer.indices = Index_Buffer*/
 
         return mesh
     }
@@ -41,7 +57,18 @@ export class Mesh{
         gl.bindTexture(gl.TEXTURE_2D, tex);
         gl.uniform1i(gl.getUniformLocation(shader.program, "tex"), 0);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer.position);
+        for(const key of Object.keys(this.buffer)){
+            if(key==="indices") continue
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer[key]);
+            var attrib = gl.getAttribLocation(shader.program, key);
+            if(attrib>-1){
+                let length = 3
+                if(key==="coord") length = 2
+                gl.vertexAttribPointer(attrib, length, gl.FLOAT, false, 0, 0);
+                gl.enableVertexAttribArray(attrib);
+            }
+        }
+        /*gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer.position);
         var position = gl.getAttribLocation(shader.program, "position");
         gl.vertexAttribPointer(position, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(position);
@@ -53,7 +80,7 @@ export class Mesh{
                 gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
                 gl.enableVertexAttribArray(coord);
             }
-        }
+        }*/
 
         var _projection = gl.getUniformLocation(shader.program, "projection");
         var _camera = gl.getUniformLocation(shader.program, "camera");
@@ -75,7 +102,7 @@ export class Mesh{
         }*/
 
         
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.buffer.index);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.buffer.indices);
         gl.drawElements(gl.TRIANGLES, this.geometry.indices.length, gl.UNSIGNED_SHORT, 0);
     }
 }
